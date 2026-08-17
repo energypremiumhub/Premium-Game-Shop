43d45b72101d3ee51760afc627b5c1e6fdd0b6ea
energypremiumhub <energypremiumhub@gmail.com>
2026-08-17 11:06:21 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
